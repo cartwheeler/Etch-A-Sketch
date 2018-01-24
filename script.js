0e076b27d285d2a46507cb730027dd39d878f701
cartwheeler <cristian_fratila@yahoo.com>
2018-01-24 16:09:48 +0200

    js file added

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('#container');
+
+const div = document.createElement('div');
+div.setAttribute('style', 'width: 16px; height: 16px');
+container.appendChild(div);
