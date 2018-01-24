36c9848847105d25206530dd89b23790110194f9
cartwheeler <cristian_fratila@yahoo.com>
2018-01-24 17:07:00 +0200

    grid layout and hover effect completed

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector('#container');
 
+for (i = 0; i < 1800; i++) {
+
 const div = document.createElement('div');
-div.setAttribute('style', 'width: 16px; height: 16px');
+div.setAttribute('style', 'width: 14px; height: 14px; display: inline-block; border: solid; border-width: 1px');
 container.appendChild(div);
+div.addEventListener("mouseenter", changeColor);
+
+function changeColor() {
+ 	div.setAttribute('style', 'width: 14px; height: 14px; display: inline-block; border: solid; border-width: 1px; background-color: grey');
+ };
+
+ };
